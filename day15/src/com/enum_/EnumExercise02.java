1ef58226c802e07eaecdb309af749a87cc9a2b5c
xmjmsc <borlow@qq.com>
2022-09-05 22:38:55 +0800

    test

---
+++ day15/src/com/enum_/EnumExercise02.java
@@ -0,0 +1,26 @@
+package com.enum_;
+
+public class EnumExercise02 {
+    public static void main(String[] args) {
+        Week[] weeks = Week.values();
+        for (Week week : weeks) {
+            System.out.println(week);
+        }
+    }
+}
+
+enum Week {
+    MONDAY("星期一"), TUESDAY("星期二"), WEDNESDAY("星期三"), THURSDAY("星期四"), FRIDAY("星期五"), SATURDAY("星期六"), SUNDAY("星期日");
+    private String name;
+
+    private Week(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Week{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
