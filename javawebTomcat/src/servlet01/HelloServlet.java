12985962ae902583c6d5d7ef93404353247bbd11
xmjmsc <borlow@qq.com>
2023-02-01 22:00:22 +0800

    servlet

---
+++ javawebTomcat/src/servlet01/HelloServlet.java
@@ -0,0 +1,32 @@
+package servlet01;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class HelloServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
